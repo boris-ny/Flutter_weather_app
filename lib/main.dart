7282ab29c3455fca83a07c0d43e608176d224457
boris-ny <b.nyilindek@alustudent.com>
2024-01-14 22:06:18 +0200

    Refactor text widget in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Hello World!',
+          ),
         ),
       ),
     );
