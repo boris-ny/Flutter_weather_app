7fb46e536e98be32cddbd292d2cfdec3c0de8eb0
jkarenzi <j.karenzi@alustudent.com>
2024-02-06 19:55:12 +0200

    pushed sign in page (not complete)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,12 +13,68 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello World!',
-          ),
-        ),
-      ),
+          backgroundColor: Colors.black12,
+          body: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Image(
+                  image: AssetImage('assets/sun.png'),
+                  width: 180,
+                  height: 140,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Sign In',
+                  style: TextStyle(fontSize: 24.0, color: Colors.white),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text("Username",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                    hintText: "Enter Username",
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text("Password",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                    hintText: "Enter Password",
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
