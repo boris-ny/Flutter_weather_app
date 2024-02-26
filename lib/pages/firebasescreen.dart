e910edaaaee6abde8cb7658f9bda7d32ff04c027
boris-ny <b.nyilindek@alustudent.com>
2024-02-27 00:53:26 +0200

    Remove unnecessary imports in firebasescreen.dart

--- lib/pages/firebasescreen.dart
+++ lib/pages/firebasescreen.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_group6_alu/firebase_options.dart';
 
 class FirebaseScreen extends StatefulWidget {
   const FirebaseScreen({super.key});
